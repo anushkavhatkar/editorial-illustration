6beb3500bab67eabc76823d2d24ce5c8f1ffed18
anushkavhatkar <vhata986@newschool.edu>
2023-10-02 16:31:30 -0400

    JS basic grid and cell setup

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ canvas.style.width = canvasDimension;
 canvas.style.height = canvasDimension;
 
 
+// 2 BASIC GRID/CELLS SETUP
+let cells = 10;
+var cellSize = canvasDimensionValue/cells;
+console.log(cellSize);
+
+canvas.style.gridTemplateColumns = `repeat(${cells}, 1fr)`;
+canvas.style.gridTemplateRows = `repeat(${cells}, 1fr)`;
+
+let xCells = cells;
+let yCells = cells;
+var totalCells = xCells*yCells;
+console.log(totalCells);
+
+
